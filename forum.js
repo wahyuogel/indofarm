7cb2de3893fbc0db2ee89b3d2715f559e002a2c6
Wahyu Aditya Nugraha <wahyuadityanugraha@rocketmail.com>
2014-10-12 00:52:28 +0700

    Initial Commit

---
+++ forum.js
@@ -0,0 +1,57 @@
+$(document)
+  .ready(function() {
+
+    $('.ui.icon')
+      .popup({
+        on: 'focus'
+      })
+    ;
+
+    $('.reply.modal')
+          .modal('setting', {
+          closable  : false,
+          onDeny    : function(){
+            window.alert('Wait not yet!');
+            return false;
+          },
+          onApprove : function() {
+            window.alert('Approved!');
+          }
+        })
+        .modal('attach events', '.reply.button', 'show')
+    ;
+
+    $('.share.modal')
+          .modal('setting', {
+          closable  : false,
+          onDeny    : function(){
+            window.alert('Wait not yet!');
+            return false;
+          },
+          onApprove : function() {
+            window.alert('Approved!');
+          }
+        })
+        .modal('attach events', '.share.button', 'show')
+    ;
+
+    $('.filter.menu .item')
+      .tab()
+    ;
+
+    $('.ui.rating')
+      .rating({
+        clearable: true
+      })
+    ;
+
+    $('.ui.sidebar')
+      .sidebar('attach events', '.launch.button')
+    ;
+
+    $('.ui.dropdown')
+      .dropdown()
+    ;
+
+  })
+;
